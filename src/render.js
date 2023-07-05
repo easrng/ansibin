030b12963bd86ea01befd53a85d7a27052377085
easrng <me@easrng.net>
2023-07-05 03:33:27 -0400

    terminal support

--- src/render.js
+++ src/render.js
@@ -1,6 +1,7 @@
 const escapeHtml = (s) =>
   s.replace(/[\x26\x0A\<>'"]/g, (s) => "&#" + s.charCodeAt(0) + ";");
-export default function render(terminal) {
+import stringWidth from "string-width";
+export function toHtml(terminal) {
   const toCSS = ([
     fg,
     bg,
@@ -15,14 +16,16 @@ export default function render(terminal) {
     strikethrough,
   ]) => {
     const colorToCSS = (c) =>
-      typeof c === "number" ? `var(--term-ansi-${c})` : c;
+      typeof c === "number" ? `var(--term-ansi-${c})` : "#" + c;
     const props = [];
     if (inverse) {
-      props.push(`color:${(bg===0||bg) ? colorToCSS(bg) : "var(--term-bg)"}`);
-      props.push(`background-color:${(fg===0||fg) ? colorToCSS(fg) : "var(--term-fg)"}`);
+      props.push(`color:${bg === 0 || bg ? colorToCSS(bg) : "var(--term-bg)"}`);
+      props.push(
+        `background-color:${fg === 0 || fg ? colorToCSS(fg) : "var(--term-fg)"}`
+      );
     } else {
-      if (fg===0||fg) props.push(`color:${colorToCSS(fg)}`);
-      if (bg===0||bg) props.push(`background-color:${colorToCSS(bg)}`);
+      if (fg === 0 || fg) props.push(`color:${colorToCSS(fg)}`);
+      if (bg === 0 || bg) props.push(`background-color:${colorToCSS(bg)}`);
     }
     if (bold) props.push("font-weight:bold");
     const decorations = [];
@@ -53,4 +56,90 @@ export default function render(terminal) {
   }
   html += "</div></pre>";
   return html;
+}
+export function toAnsi(terminal) {
+  const toEscape = ([
+    fg,
+    bg,
+    inverse,
+    bold,
+    underline,
+    overline,
+    blink,
+    invisible,
+    italic,
+    dim,
+    strikethrough,
+  ]) => {
+    let extra = "";
+    const props = ["0"];
+    //    if (fg === 0|| fg) props.push(`color:${colorToCSS(fg)}`);
+    //    if (bg === 0|| bg) props.push(`background-color:${colorToCSS(bg)}`);
+    if (inverse) props.push("7");
+    if (bold) props.push("1");
+    if (underline) props.push("4");
+    if (overline) props.push("53");
+    if (blink) props.push("5");
+    if (strikethrough) props.push("9");
+    if (invisible) props.push("8");
+    if (italic) props.push("3");
+    if (dim) props.push("2");
+    const colorToAnsi = (code, color) => {
+      if (typeof color === "number") {
+        color += "";
+        if (color.length === 1) {
+          props.push(code + color);
+        } else {
+          extra += "\x1b[" + code + "8;5;" + color + "m";
+        }
+      } else if (color && typeof color === "string") {
+        const [r, g, b] = Buffer.from(color, "hex");
+        extra +=
+          "\x1b[" +
+          code +
+          "8;2;" +
+          (r || 0) +
+          ";" +
+          (g || 0) +
+          ";" +
+          (b || 0) +
+          "m";
+      }
+    };
+    colorToAnsi("3", fg);
+    colorToAnsi("4", bg);
+    return "\x1b[" + props.join(";") + "m" + extra;
+  };
+  let ansi = "";
+  for (const row of terminal) {
+    let style = [];
+    for (let seg of row) {
+      if (typeof seg === "string") {
+        ansi += toEscape(style) + seg;
+      } else {
+        style = seg;
+      }
+    }
+    ansi += toEscape([]) + "\n";
+  }
+  let lines = ansi.split("\n");
+  const widths = lines.map((e) => stringWidth(e));
+  const widest = widths.reduce((a, b) => (a > b ? a : b), 0);
+  lines = lines.map((line, i) => line + " ".repeat(widest - widths[i]));
+  const title = " ansibin ";
+  const header =
+    "\x1b[2m╭" +
+    "─".repeat(Math.floor((widest - title.length) / 2)) +
+    "\x1b[0m" +
+    title +
+    "\x1b[2m" +
+    "─".repeat(Math.ceil((widest - title.length) / 2)) +
+    "╮\x1b[0m\n";
+  const footer = "\x1b[2m╰" + "─".repeat(widest) + "╯\x1b[0m\n";
+  return (
+    header +
+    lines.map((line) => "\x1b[2m│" + line + "\x1b[2m│\x1b[0m").join("\n") +
+    "\n" +
+    footer
+  );
 }
--- src/server.js
+++ src/server.js
@@ -1,23 +1,42 @@
 import express from "express";
 import template from "./template.js";
-import render from "./render.js";
+import * as render from "./render.js";
 import test from "../public/test.json" assert { type: "json" };
-import fs from "fs/promises"
+import fs from "fs/promises";
 const app = express();
 app.use(express.static("public"));
+/* async */ function send(o, req, res) {
+  //let render;
+  //try {
+  //  render = await import("./render.js?" + Date.now());
+  //} catch (e) {
+  //  res.send("Failed to load renderer.\n" + e);
+  //  return;
+  //}
+  const html = (req.headers.accept || "").includes("text/html");
+  if (html) {
+    res.header("content-type", "text/html");
+    res.send(template(render.toHtml(o)));
+  } else {
+    res.header("content-type", "text/plain");
+    res.send(render.toAnsi(o));
+  }
+}
 app.get("/v/:filename", async (req, res) => {
   let o;
   try {
-    if(req.params.filename.match(/[\\/]/)) throw new Error();
-    o = JSON.parse((await fs.readFile("./recordings/"+req.params.filename)).toString());
+    if (req.params.filename.match(/[\\/]/)) throw new Error();
+    o = JSON.parse(
+      (await fs.readFile("./recordings/" + req.params.filename)).toString()
+    );
   } catch (e) {
-    console.log(e)
+    console.log(e);
     o = [["Error."]];
   }
-  res.send(template(render(o)));
+  send(o, req, res);
 });
 app.get("/", (req, res) => {
-  res.send(template(render(test)));
+  send(test, req, res);
 });
 const listener = app.listen(3019, "127.0.0.1", () => {
   console.log(`Your app is listening on port ${listener.address().port}`);
